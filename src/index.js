97bc89d7e1a03d1bdc80dc3e9235d955f194ad68
Deepika Verma <65213238+deepikaverma710@users.noreply.github.com>
2021-04-12 11:24:28 -0700

    Update index.js

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import ContextIndex from './context/ContextIndex';
+import ContextIndex from './context/contextIndex';
 import {BrowserRouter} from 'react-router-dom';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
