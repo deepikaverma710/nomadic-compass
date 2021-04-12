cb541dc7835f4a4f7a447e3b298a203c425e6f01
KulveerBrar <brarkulveer15@gmail.com>
2021-04-12 03:41:55 -0700

    add private route #4

--- src/App.js
+++ src/App.js
@@ -1,18 +1,30 @@
-import React from 'react';
-import { Route, Switch} from 'react-router-dom'
+import React, {useContext} from 'react';
+import { Redirect,Route, Switch} from 'react-router-dom'
+import {firebaseAuth} from './context/ContextIndex'
 import Register from './components/Register'
 import Home from './components/Home'
 import Login from './components/Login'
 
+function PrivateRoute({component: Component, token, ...rest}) {
+  return(
+  <Route
+  {...rest}
+  render={(props) => token != null  
+    ? <Component {...props} {...rest} />
+    : <Redirect to ='/login' />} />
+  )
+}
 
 function App() {
+const { token } = useContext(firebaseAuth)
+console.log(token)
 
-  return (
-    <Switch>
-    <Route exact path='/' component={Home} />
-    <Route exact path='/register' component={Register} />
-    <Route exact path='/login' component={Login} />
-    </Switch>
+return (
+<Switch>
+  <PrivateRoute exact path='/' component = {Home} token={token} />
+  <Route exact path='/register' component={Register} />
+  <Route exact path='/login' component={Login} />
+  </Switch>
   );
 }
 
