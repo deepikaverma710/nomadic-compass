ee2e1b086c32899e1b22deb86322796f99be6808
KulveerBrar <brarkulveer15@gmail.com>
2021-04-15 00:31:51 -0700

    add receipt model #15

--- src/pages/cart/Cart.js
+++ src/pages/cart/Cart.js
@@ -1,142 +1,189 @@
-import React, {useState} from 'react';
+import React, { useState } from 'react';
 import Navigation from '../../components/navigation/Navigation';
 import { FaPlusCircle, FaMinusCircle } from "react-icons/fa";
+import { Link } from "react-router-dom";
+import { Modal, Button } from 'react-bootstrap'
 
 const Cart = () => {
-    const tourPackages= [{
-        "_id": "60760e8143090445b5f0811f",
-        "packageName": "new package",
-        "packageDescription": "Description",
-        "packageHighlights": "Highlights",
-        "startLocation": "start here",
-        "finishLocation": "finish here",
-        "groupSize": 5,
-        "activityLevel": "Easy",
-        "duration": "5",
-        "budget": 10000,
-        "meals": "Breakfast, Lunch",
-        "transport": "Bus",
-        "accommodation": "Hotel, Camps",
-        "dates": [
-            {
-              "numberOfPersonJoined": 1,
-                "_id": "60760e8143090445b5f08120",
-                "departureDate": "2021-04-13T21:34:57.104Z",
-                "tripStatus": "Active"
-            },
-            {
-                "numberOfPersonJoined": 2,
-                  "_id": "60760e8143090445b5f08121",
-                  "departureDate": "2021-05-13T21:34:57.104Z",
-                  "tripStatus": "Active"
-              },
-              {
-                "numberOfPersonJoined": 3,
-                  "_id": "60760e8143090445b5f08122",
-                  "departureDate": "2021-06-13T21:34:57.104Z",
-                  "tripStatus": "Active"
-              }]
-    },
-    {
-        "_id": "60760e8143090445b5f0811g",
-        "packageName": "new package 2",
-        "packageDescription": "Description 2",
-        "packageHighlights": "Highlights",
-        "startLocation": "start here",
-        "finishLocation": "finish here",
-        "groupSize": 6,
-        "activityLevel": "Easy",
-        "duration": "5",
-        "budget": 10000,
-        "meals": "Breakfast, Lunch",
-        "transport": "Bus",
-        "accommodation": "Hotel, Camps",
-        "dates": [
-            {
-              "numberOfPersonJoined": 1,
-                "_id": "60760e8143090445b5f08120",
-                "departureDate": "2021-04-13T21:34:57.104Z",
-                "tripStatus": "Active"
-            },
-            {
-                "numberOfPersonJoined": 2,
-                  "_id": "60760e8143090445b5f08121",
-                  "departureDate": "2021-05-13T21:34:57.104Z",
-                  "tripStatus": "Active"
-              },
-              {
-                "numberOfPersonJoined": 3,
-                  "_id": "60760e8143090445b5f08122",
-                  "departureDate": "2021-06-13T21:34:57.104Z",
-                  "tripStatus": "Active"
-              }]
-    }]
-    const [qty, setQty] = useState(1)
-    const [minusButton, setMinusButton] = useState(false)
-    const [plusButton, setPlusButton] = useState(false)
-    const qtyAddtion =() => {
-        setMinusButton(false)
-        const newQty= qty+1;
-        setQty(newQty);
-    }
-
-    const qtySubtraction=()=>{
-        if (qty<2){
-            setMinusButton(true)
+    const tourPackage = [
+        {
+            _id: "60760e8143090445b5f0811d",
+            activityName: "New activiity ",
+            destinations: [
+                {
+                    _id: "60760e8143090445b5f0811e",
+                    destinationName: "new",
+                    tourPackages: [
+                        {
+                            _id: "60760e8143090445b5f0811f",
+                            packageName: "new package",
+                            packageDescription: "Description",
+                            packageHighlights: "Highlights",
+                            startLocation: "start here",
+                            finishLocation: "finish here",
+                            groupSize: 5,
+                            activityLevel: "Easy",
+                            duration: "5",
+                            budget: 10000,
+                            inclusions: "ABC, DEF",
+                            dates: [
+                                {
+                                    noOfPackageAdded: 2,
+                                    _id: "60760e8143090445b5f08120",
+                                    departureDate: "2021-04-13T21:34:57.104Z",
+                                    tripStatus: "Active"
+                                }
+                            ]
+                        }
+                    ]
+                }
+            ],
+        },
+        {
+            _id: "60760e8143090445b5f0811e",
+            activityName: "New activiity 2",
+            destinations: [
+                {
+                    _id: "60760e8143090445b5f0811e",
+                    destinationName: "new dest 2",
+                    tourPackages: [
+                        {
+                            _id: "60760e8143090445b5f0811f",
+                            packageName: "new package 2",
+                            packageDescription: "Description",
+                            packageHighlights: "Highlights",
+                            startLocation: "start here",
+                            finishLocation: "finish here",
+                            groupSize: 5,
+                            activityLevel: "Easy",
+                            duration: "5",
+                            budget: 20000,
+                            inclusions: "ABC, DEF",
+                            dates: [
+                                {
+                                    noOfPackageAdded: 3,
+                                    _id: "60760e8143090445b5f08120",
+                                    departureDate: "2021-04-13T21:34:57.104Z",
+                                    tripStatus: "Active"
+                                }
+                            ]
+                        }
+                    ]
+                }
+            ],
         }
-        else{
-        const newQty=qty-1;
-        setQty(newQty);
-        }
-    }
+    ]
+
+    let total = 0
+    const [show, setShow] = useState(false);
+
+  const handleClose = () => setShow(false);
+  const handleShow = () => setShow(true);
+
+    // const [qty, setQty] = useState(1)
+    // const [totalPrice, setTotalPrice] = useState()
+    // const [minusButton, setMinusButton] = useState(false)
+    // const [plusButton, setPlusButton] = useState(false)
+    // const qtyAddtion = () => {
+    //     setMinusButton(false)
+    //     const newQty = qty + 1;
+    //     setQty(newQty);
+    // }
+
+    // const qtySubtraction = () => {
+    //     if (qty < 2) {
+    //         setMinusButton(true)
+    //     }
+    //     else {
+    //         const newQty = qty - 1;
+    //         setQty(newQty);
+    //     }
+    // }
 
 
 
-  return (
-      <div className="hero-image">  
-        <Navigation/>
-        <table className="table bg-white">
-  <thead>
-    <tr>
-      <th scope="col">Package</th>
-      <th scope="col">Destination</th>
-      <th scope="col">Activity</th>
-      <th scope="col">Qty</th>
-      <th scope="col">Price/Person</th>
-      <th scope="col">Total Price</th>
-    </tr>
-  </thead>
-  <tbody>
-  {tourPackages.map (a=>
-    <tr>
-      <th scope="row">{a.packageName}</th>
-      <td>Manali</td>
-      <td>Camping</td>
-      <td>
-          <button 
-          disabled={minusButton} 
-          className= "pb-1 border-0 bg-white" 
-          onClick={qtySubtraction}>
-              <FaMinusCircle />
-              </button >
-              {qty}
-              <button 
-              disabled={plusButton}
-              className="border-0 bg-white" 
-              onClick={qtyAddtion}>
-                  <FaPlusCircle />
-                  </button>
-                  </td>
-      <td>{a.budget}</td>
-      <td>{qty*a.budget}</td>
-      <td className="text-right"><button>See details</button><button>Remove</button></td>
-    </tr>
-    )}
-  </tbody>
-</table>
-       
+    return (
+        <div>
+            <div className="hero-image">
+                <Navigation />
+            </div>
+            <div className="table-responsive-lg">
+                <table className="table">
+
+
+                    <thead>
+                        <tr>
+                            <th scope="col">Package</th>
+                            <th scope="col">Destination</th>
+                            <th scope="col">Activity</th>
+                            {/* <th scope="col">Qty</th> */}
+                            <th scope="col">Price/Person</th>
+                            {/* <th scope="col">Total Price</th> */}
+                        </tr>
+                    </thead>
+                    <tbody>
+                        {tourPackage.map(a =>
+                            <tr key={a._id}>
+                                <th scope="row">{a.destinations[0].tourPackages[0].packageName}</th>
+                                <td>{a.destinations[0].destinationName}</td>
+                                <td>{a.activityName}</td>
+                                {/* <td>
+                                    <button
+                                        disabled={minusButton}
+                                        className="pb-1 border-0 bg-white"
+                                        onClick={qtySubtraction}>
+                                        <FaMinusCircle />
+                                    </button >
+                                    {a.destinations[0].tourPackages[0].dates[0].noOfPackageAdded}
+                                    <button
+                                        disabled={plusButton}
+                                        className="border-0 bg-white"
+                                        onClick={()=>a.destinations[0].tourPackages[0].dates[0].noOfPackageAdded =a.destinations[0].tourPackages[0].dates[0].noOfPackageAdded+1}>
+                                        <FaPlusCircle />
+                                    </button>
+                                </td> */}
+                                <td className="d-none">{total = total + a.destinations[0].tourPackages[0].budget}</td>
+                                <td>{a.destinations[0].tourPackages[0].budget}</td>
+                                {/* <td>{qty * a.destinations[0].tourPackages[0].budget}</td> */}
+                                <td className="text-right"><Link to="/details"><button>See details</button> </Link><button>Remove</button></td>
+                            </tr>
+                        )}
+                    </tbody>
+                </table>
+            </div>
+            {/* <div className=" text-center font-weight-bold">
+               <button>Checkout</button>
+            </div> */}
+            <div className="border p-5 text-center font-weight-bold">
+                <div className=" pb-5">Total: ${total}</div>
+   <Button variant="secondary" onClick={handleShow}>
+   Checkout
+      </Button>
       </div>
-  );
+
+      <Modal show={show} onHide={handleClose}>
+        <Modal.Header closeButton>
+          <Modal.Title>Receipt</Modal.Title>
+        </Modal.Header>
+        <Modal.Body>
+            <ul className="list-unstyled">
+            {tourPackage.map(a =>
+            <li >
+                {a.destinations[0].tourPackages[0].packageName}
+                </li>
+                )}
+                <li className= "font-weight-bold">Total Price= {total}</li>
+                </ul>
+                </Modal.Body>
+        <Modal.Footer>
+          <Button variant="secondary" onClick={handleClose}>
+            Close
+          </Button>
+        </Modal.Footer>
+      </Modal>
+
+        </div>
+    );
 
 };
 
